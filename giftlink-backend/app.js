356a22a0b98186a485388eaf6a1b0152c2e8020d
AbdurRafay2000 <rafayalisatti787@gmail.com>
2026-09-06 15:44:44 -0400

    Implement search endpoint and register search routes in app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,9 +7,8 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
-
 const app = express();
-app.use("*",cors());
+app.use("*", cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
@@ -18,41 +17,36 @@ connectToDatabase().then(() => {
 })
     .catch((e) => console.error('Failed to connect to DB', e));
 
-
 app.use(express.json());
-
-// Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
-const giftRoutes = require('./routes/giftRoutes');
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
-
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
+// Route files
+// Gift API Task 1: import the giftRoutes and store in a constant called giftRoutes
+const giftRoutes = require('./routes/giftRoutes');
+
+// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
+const searchRoutes = require('./routes/searchRoutes');
+
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
+// Search API Task 2: MUST be placed BEFORE giftRoutes so /api/gifts/search isn't treated as an ID parameter
+app.use('/api/gifts/search', searchRoutes);
 
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
-
-
+// Gift API Task 2: add the giftRoutes to the server
 app.use('/api/gifts', giftRoutes);
+
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
 });
 
-app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
+app.get("/", (req, res) => {
+    res.send("Inside the server");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
